2fe9e56941b906b44e64074cc2963ec945d59719
AAKKUS1 <aakkus@edenschool.fr>
2024-11-25 15:32:02 +0100

    fix(app)ex02

--- ex.02.js
+++ ex.02.js
@@ -3,7 +3,7 @@ function countEvenNumbers(arr) {
     let result = 0
         for (let i = 0;i < arr.length; i++){
           if (arr[i] % 2 === 0){
-            result ++
+            result++
           }
         }
     return result
