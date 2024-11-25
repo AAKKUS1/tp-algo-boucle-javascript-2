d7f3ddbcd7a0f49be7f3b67efcef481fdd006ffd
AAKKUS1 <aakkus@edenschool.fr>
2024-11-25 15:21:40 +0100

    feat(js.01)function-error

--- ex01.js
+++ ex01.js
@@ -1,13 +1,16 @@
-let result = arr[0];
-  for (let i = 0;i < arr.length; i++){
-   if (result < arr[i]){
-     result = arr[i]
-   }
+
+
+function findMax(arr) {
+    let result = arr[0];
+    for (let i = 0;i < arr.length; i++){
+     if (result < arr[i]){
+       result = arr[i]
+     }
+    }
+    return result
   }
-  return result
-
-
-
-console.log(findMax([1, 3, 7, 2])); // 
-console.log(findMax([-5, -1, -10]));
-
+  
+    
+  
+  console.log(findMax([1, 3, 7, 2])); // 
+  console.log(findMax([-5, -1, -10]));
